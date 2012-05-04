cbe1e0995d2ce2bca1bbe58120890a91ddd9bd8d
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/JQTablePlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/JQTablePlugin.pm
+++ lib/Foswiki/Plugins/JQTablePlugin.pm
@@ -4,13 +4,14 @@ package Foswiki::Plugins::JQTablePlugin;
 use strict;
 use Assert;
 
-use Foswiki::Func ();       # The plugins API
-use Foswiki::Plugins ();    # For the API version
+use Foswiki::Func                  ();    # The plugins API
+use Foswiki::Plugins               ();    # For the API version
 use Foswiki::Plugins::JQueryPlugin ();
 
 our $VERSION = '$Rev: 5771 $';
 our $RELEASE = '1.1.2';
-our $SHORTDESCRIPTION = 'Javascript implementation of the classic TablePlugin, using JQuery';
+our $SHORTDESCRIPTION =
+  'Javascript implementation of the classic TablePlugin, using JQuery';
 our $NO_PREFS_IN_TOPIC = 1;
 our $DEFAULT_TABLE_SETTINGS =
 'tableborder="1" valign="top" headercolor="#fff" headerbg="#687684" headerbgsorted="#345" databg="#fff,#edf4f9" databgsorted="#f1f7fc,#ddebf6" tablerules="rows" headerrules="cols"';
@@ -19,36 +20,37 @@ our %pluginAttributes;
 sub initPlugin {
     my ( $topic, $web, $user, $installWeb ) = @_;
 
-    Foswiki::Plugins::JQueryPlugin::registerPlugin(
-        'tablesorter', 'Foswiki::Plugins::JQTablePlugin::TableSorter');
+    Foswiki::Plugins::JQueryPlugin::registerPlugin( 'tablesorter',
+        'Foswiki::Plugins::JQTablePlugin::TableSorter' );
 
     my $src = DEBUG() ? '.uncompressed' : '';
     my $sort = Foswiki::Func::getPreferencesValue('TABLEPLUGIN_SORT') || 'all';
 
-    if ($sort =~ /^(all|attachments)/) {
+    if ( $sort =~ /^(all|attachments)/ ) {
+
         # In this special case, the tablesorter is always required
         Foswiki::Plugins::JQueryPlugin::createPlugin('tablesorter');
-        Foswiki::Func::addToZone('head', 'JQTABLEPLUGIN::SORT', <<HERE);
+        Foswiki::Func::addToZone( 'head', 'JQTABLEPLUGIN::SORT', <<HERE);
 <meta name="foswiki.JQTablePlugin.sort" content="$sort" />
 HERE
     }
 
     # if the TablePlugin isn't there to handle table formatting, then
     # enable JS to do it.
-    if (!$Foswiki::cfg{Plugins}{TablePlugin}{Enabled}) {
+    if ( !$Foswiki::cfg{Plugins}{TablePlugin}{Enabled} ) {
         Foswiki::Func::registerTagHandler( 'TABLE', \&_TABLE );
     }
     return 1;
 }
 
 sub _TABLE {
-    my($session, $params, $topic, $web) = @_;
+    my ( $session, $params, $topic, $web ) = @_;
 
     Foswiki::Plugins::JQueryPlugin::createPlugin('tablesorter');
 
     _readPluginSettings() if !%pluginAttributes;
     my %p = %pluginAttributes;
-    while (my ($k, $v) = each %$params) {
+    while ( my ( $k, $v ) = each %$params ) {
         next if $k =~ /^_/ && $k ne '_DEFAULT';
         $p{$k} = $v;
     }
@@ -56,11 +58,13 @@ sub _TABLE {
         {
             class => 'jqtp_process',
             style => 'display:none',
+
             # Note: manual JSON generator, due to amazing slowness
             # of CPAN JSON module
-            title => join(',', map { "'$_':'$p{$_}'" } keys %p),
+            title => join( ',', map { "'$_':'$p{$_}'" } keys %p ),
         },
-        '');
+        ''
+    );
 }
 
 # Copied from TablePlugin
@@ -72,9 +76,9 @@ sub _readPluginSettings {
 
     $configureAttrStr ||= $DEFAULT_TABLE_SETTINGS;
 
-    $configureAttrStr = Foswiki::Func::expandCommonVariables( $configureAttrStr )
+    $configureAttrStr = Foswiki::Func::expandCommonVariables($configureAttrStr)
       if $configureAttrStr;
-    $pluginAttrStr = Foswiki::Func::expandCommonVariables( $pluginAttrStr )
+    $pluginAttrStr = Foswiki::Func::expandCommonVariables($pluginAttrStr)
       if $pluginAttrStr;
 
     my %configureParams = Foswiki::Func::extractParameters($configureAttrStr);
--- lib/Foswiki/Plugins/JQTablePlugin/TableSorter.pm
+++ lib/Foswiki/Plugins/JQTablePlugin/TableSorter.pm
@@ -4,18 +4,19 @@ use Foswiki::Plugins::JQueryPlugin::Plugin ();
 our @ISA = 'Foswiki::Plugins::JQueryPlugin::Plugin';
 
 sub new {
-    my $class = shift;
+    my $class   = shift;
     my $session = shift || $Foswiki::Plugins::SESSION;
-    my $this = bless($class->SUPER::new(
-        $session,
-        name => 'TableSorter',
-        version => '2.0.3',
-        author => 'Christian Bach',
-        homepage => 'http://tablesorter.com/docs/',
-        puburl => '%PUBURLPATH%/%SYSTEMWEB%/JQTablePlugin',
-        css => [ 'jquery.tablesorter.css' ],
-        javascript => [ 'jquery.tablesorter.js', 'tables.js'],
-        summary => <<SUMMARY), $class);
+    my $this    = bless(
+        $class->SUPER::new(
+            $session,
+            name       => 'TableSorter',
+            version    => '2.0.3',
+            author     => 'Christian Bach',
+            homepage   => 'http://tablesorter.com/docs/',
+            puburl     => '%PUBURLPATH%/%SYSTEMWEB%/JQTablePlugin',
+            css        => ['jquery.tablesorter.css'],
+            javascript => [ 'jquery.tablesorter.js', 'tables.js' ],
+            summary    => <<SUMMARY), $class );
 <nop>TableSorter is a jQuery plugin for turning a standard HTML table with THEAD and
 TBODY tags into a sortable table without page refreshes. tablesorter can
 successfully parse and sort many types of data including linked data in a cell.
